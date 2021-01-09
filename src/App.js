b87e1680c2785ffb7132d6827ae71d4546b2313e
Mahesh <Mahesh.lkp1234@gmail.com>
2021-01-08 20:34:06 -0600

    changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
-function App() {
+function App () {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    // console.log(`this the the input value ${input}`)
+    setTodos([...todos, input]);
+    setInput("");
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to my Todolist</h1>
+        <form>
+          <input value={input} onChange={(e) => setInput(e.target.value)} type="text" />
+          <button onClick={addTodo}>Add Todo</button>
+        </form>
+
+        <h2>List of todos</h2>
+        {todos.map((todo) => (
+          <p>{todo}</p>
+        ))}
       </header>
     </div>
   );
