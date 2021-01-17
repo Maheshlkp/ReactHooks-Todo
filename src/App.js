5d50ce0b78038e5eb2369ba7f678fe69aa9f97d4
Mahesh <Mahesh.lkp1234@gmail.com>
2021-01-16 22:15:23 -0600

    added changes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,20 @@
 import './App.css';
-import React, { useState } from "react";
+import React from "react";
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App () {
-  const [todos, setTodos] = useState([]);
-  const [input, setInput] = useState("");
+  const { isLoading } = useAuth0();
 
-  const addTodo = (e) => {
-    e.preventDefault();
-    // console.log(`this the the input value ${input}`)
-    setTodos([...todos, input]);
-    setInput("");
-  }
-
+  if (isLoading) return <div>Loading...</div>
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to my Todolist</h1>
-        <form>
-          <input value={input} onChange={(e) => setInput(e.target.value)} type="text" />
-          <button onClick={addTodo}>Add Todo</button>
-        </form>
-
-        <h2>List of todos</h2>
-        {todos.map((todo) => (
-          <p>{todo}</p>
-        ))}
-      </header>
-    </div>
+    <>
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
+    </>
   );
 }
 
